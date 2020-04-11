e836083ce5c154df10995c1e4fefc207dbf34ac1
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2020-04-10 21:07:50 -0300

    ref: convert config to object

--- promptConfig.ts
+++ promptConfig.ts
@@ -1,5 +1,5 @@
-export const promptConfig = [
-  {
+export const promptConfig = {
+  start: {
     name: 'start',
     type: 'list',
     message: 'What you wanna do?',
@@ -14,34 +14,34 @@ export const promptConfig = [
       },
     ],
   },
-  {
+  userName: {
     name: 'userName',
     type: 'input',
     message: 'Enter your name',
     validate: (value: string) => value.length === 0 ? 'A name is required' : true,
   },
-  {
+  gameName: {
     name: 'gameName',
     type: 'input',
     message: 'New game name',
     validate: (value: string) => value.length === 0 ? 'A name is required' : true,
   },
-  {
+  rows: {
     name: 'rows',
     type: 'list',
     message: 'How many rows?',
     choices: ['5', '10', '20'],
   },
-  {
+  columns: {
     name: 'columns',
     type: 'list',
     message: 'How many columns?',
     choices: ['5', '10', '20'],
   },
-  {
+  mines: {
     name: 'mines',
     type: 'list',
     message: 'How many mines?',
     choices: ['5', '10', '20'],
   }
-]
+}
